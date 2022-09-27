f89d63caf96d906e7f218eb040ec63418ab2e3bf
Rafael <raf.nascimento01@gmail.com>
2022-09-27 16:37:24 -0300

    Adicionando dll de log de exceções e aplicando

---
+++ Bibliotecas/LogExcecoes/TratamentoExcecoes.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace LogExcecoes
+{   /// <summary>
+    /// Classe para catalogar exceções
+    /// </summary>
+    public class TratamentoExcecoes
+    {
+        string caminhoFisico = HttpContext.Current.Server.MapPath("~/Erros.txt");
+   
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ex">Recebe uma Exception como parametro</param>
+        public void SalvarExececoes(Exception ex) {
+            string conteudo = DateTime.Now.ToString() + " - " + ex.Message + "\n";
+            //Rastreia métodos executados até o momento do errro
+            conteudo += ex.StackTrace;
+            //Source mostra o arquivo onde houve o erro
+            conteudo += ex.Source;
+            conteudo += "\n----------------------------------------------------\n";
+            System.IO.File.AppendAllText(caminhoFisico, conteudo);
+        }
+        /// <summary>
+        /// Ler todas as exceções no log
+        /// </summary>
+        /// <returns></returns>
+        public string LerExcecoes() {
+            return File.ReadAllText(caminhoFisico).Replace("\n", "<br>");    
+        }
+        /// <summary>
+        /// Limpar o log do exceções
+        /// </summary>
+        public void LimparExcecoes() {
+            File.WriteAllText(caminhoFisico, "");
+        }
+    }
+}
---
+++ c-sharp-web/Projeto3/ExibirExcecoes.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using LogExcecoes;
+
+namespace Projeto3 {
+    public partial class ExibirExcecoes1 : System.Web.UI.Page {
+        string caminhoFisico = HttpContext.Current.Server.MapPath("~/Erros.txt");
+
+        protected void Page_Load(object sender, EventArgs e) {
+            var te = new TratamentoExcecoes();
+            Excecoes.Text = te.LerExcecoes();
+        }
+
+        protected void Limpar_Click(object sender, EventArgs e) {
+            var te = new TratamentoExcecoes();
+            te.LimparExcecoes();
+            Excecoes.Text = "";
+        }
+    }
+}
